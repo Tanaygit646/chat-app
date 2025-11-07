efc8674a8101d80b0e1911af58567046379c08c5
Tanaygit646 <tanay90nta@gmail.com>
2025-11-07 12:23:29 +0530

    Implement the basic code of socket.io

---
+++ index.js
@@ -0,0 +1,24 @@
+const http = require("http");
+const path = require("path");
+const express = require("express");
+const app = express();
+const server = http.createServer(app);
+const {Server} = require("socket.io")
+
+app.use(express.static(path.resolve("./public")));
+
+const io = new Server(server);
+
+io.on("connection",(socket)=>{
+    socket.on("user-message",(message)=>{
+        console.log("A new user message:-",message);
+        io.emit("message",message)
+    })
+})
+
+app.get("/",(req,res)=>{
+    return res.sendFile("/public/index.html")
+})
+
+const port = 2000;
+server.listen(port,()=> console.log(`http://localhost:${port}`));
